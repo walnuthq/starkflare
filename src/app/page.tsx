daa3b0451028cf2195a57ed36d2c86c45c1aea23
Roman <30427286+barabanovro@users.noreply.github.com>
2024-05-23 13:05:52 +0400

    fix: Disable API request cache (#17)
    * Disable cache
    
    * Set edge runtime

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+export const runtime = 'edge'
+
 import HomePage from '@/components/home-page'
 import { CommonStats } from '@/lib/types'
 import camelcaseKeys from 'camelcase-keys'
 
 async function getCommonStats() {
-	const res = await fetch('http://starkf-servi-1yasdoedwfh1-321738969.us-east-1.elb.amazonaws.com/rpc/get_common_data')
+	const res = await fetch('http://starkf-servi-1yasdoedwfh1-321738969.us-east-1.elb.amazonaws.com/rpc/get_common_data', {
+		cache: 'no-store',
+	})
 	const commonStats: CommonStats = camelcaseKeys((await res.json()) as any, { deep: true })
 	return commonStats
 }
