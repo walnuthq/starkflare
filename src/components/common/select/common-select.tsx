8ffd0a7a55e5db78dee630da4e0995169363b7b0
bhavyagosai <bhavyagosai4@gmail.com>
2024-05-30 05:20:19 +0530

    add: extended ui components

---
+++ src/components/common/select/common-select.tsx
@@ -0,0 +1,56 @@
+import {
+  SelectItem,
+  Select,
+  SelectContent,
+  SelectTrigger,
+  SelectValue,
+  SelectGroup,
+  SelectLabel,
+} from '@/components/ui'
+
+type Items = {
+  label?: string
+  items: string[]
+}
+
+type Props = {
+  className?: string
+  placeholder?: string
+  onValueChange?: (value: string) => void
+  defaultValue: string
+  contents: Items[]
+}
+
+export function CommonSelect(props: Props) {
+  const { className, placeholder, contents, onValueChange, defaultValue } =
+    props
+  return (
+    <Select onValueChange={onValueChange} defaultValue={defaultValue}>
+      <SelectTrigger className={className}>
+        <SelectValue placeholder={placeholder} />
+      </SelectTrigger>
+      {/* set a common max height for dropdown */}
+      <SelectContent className="max-h-56 overflow-y-auto">
+        <SelectGroup>
+          {contents.map((content) => {
+            return (
+              <>
+                {content.label && <SelectLabel>{content.label}</SelectLabel>}
+                {content.items.map((item) => {
+                  return (
+                    <SelectItem
+                      className="cursor-pointer"
+                      value={item.toLowerCase()}
+                    >
+                      {item}
+                    </SelectItem>
+                  )
+                })}
+              </>
+            )
+          })}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  )
+}
---
+++ src/components/common/tooltip/common-tooltip.tsx
@@ -0,0 +1,48 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
+import { ReactNode, useRef } from 'react'
+
+type Props = {
+  className?: string
+  children: ReactNode
+  tooltipMessage: ReactNode
+  asChild?: boolean
+  hideOnClick?: boolean
+}
+
+export function CommonTooltip(props: Props) {
+  const { className, children, tooltipMessage, asChild, hideOnClick } = props
+  const triggerRef = useRef(null)
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger
+          className={className}
+          asChild={asChild}
+          onClick={(event) => {
+            if (!hideOnClick) event.preventDefault()
+          }}
+          ref={triggerRef}
+        >
+          {children}
+        </TooltipTrigger>
+        <TooltipContent
+          onPointerDownOutside={(event) => {
+            if (event.target === triggerRef.current && !hideOnClick)
+              event.preventDefault()
+          }}
+        >
+          {tooltipMessage}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+}
+
+CommonTooltip.defaultProps = {
+  hideOnClick: true,
+}
