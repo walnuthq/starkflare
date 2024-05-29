fe7cf91d75dd9acc9ffe09c42efe5194bcbcecf1
bhavyagosai <bhavyagosai4@gmail.com>
2024-05-30 05:23:04 +0530

    add: helper functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatNumber(number: number): string {
+  if (number >= 1_000_000) {
+    return `${(number / 1_000_000).toFixed(1)}M`
+  } else if (number >= 1_000) {
+    return `${(number / 1_000).toFixed(1)}k`
+  } else {
+    return number.toString()
+  }
+}
+
+export async function copyToClipboard(text: string): Promise<void> {
+  try {
+    await navigator.clipboard.writeText(text)
+  } catch (err) {
+    console.error('Could not copy text: ', err)
+    throw err
+  }
+}
