39f4396d5f446000ed4f3aac8bd91960a9d8f866
Simon <saimeunt@gmail.com>
2024-05-30 17:13:32 +0200

    add entrypoints

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -13,3 +13,21 @@ export interface CommonStats {
   userStats: UserStats
   transactionStats: TransactionStats
 }
+
+export interface RawEntrypoint {
+  entrypointSelector: string
+  entrypointSteps: number
+  entrypointStepsPercentage: number
+}
+
+export interface RawEntrypoints {
+  entrypoints: RawEntrypoint[]
+}
+
+export interface Entrypoint extends RawEntrypoint {
+  entrypoint: string
+}
+
+export interface Entrypoints {
+  entrypoints: Entrypoint[]
+}
