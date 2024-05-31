17fea36299e2503fae9881d1cd13d9023e29857f
BlackStarkGoku <165695008+BlackStarkGoku@users.noreply.github.com>
2024-05-31 12:12:05 +0200

    Update: Fix contribute button

--- src/components/ContributeBox.tsx
+++ src/components/ContributeBox.tsx
@@ -1,21 +1,19 @@
-import React from "react";
-import { Button } from "./ui/button";
-import Link from "next/link";
-import { GITHUB_REPO_URL } from "@/utils/constants";
+import React from 'react'
+import { Button } from './ui/button'
+import Link from 'next/link'
+import { GITHUB_REPO_URL } from '@/utils/constants'
 
 function ContributeBox() {
   return (
     <div className="flex flex-col items-center">
       <h2 className="mb-10 font-medium text-lg md:text-xl">
-        Have ideas to make cairovm.codes better?
+        Have ideas to make Starkflare better?
       </h2>
       <Link href={GITHUB_REPO_URL}>
-        <Button size={"padded"} variant={"contribute"}>
-          Contribute on GitHub
-        </Button>
+        <Button>Contribute on GitHub</Button>
       </Link>
     </div>
-  );
+  )
 }
 
-export default ContributeBox;
+export default ContributeBox
