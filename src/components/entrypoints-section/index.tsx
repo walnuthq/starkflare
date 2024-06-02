c9988a85bbc4c33e7bca52ae30f638ff676df594
bhavyagosai <bhavyagosai4@gmail.com>
2024-06-02 06:20:28 +0530

    fix: align tables :: better ui

--- src/components/entrypoints-section/index.tsx
+++ src/components/entrypoints-section/index.tsx
@@ -24,7 +24,7 @@ export function EntrypointsSection(props: EntrypointsSectionProps) {
 
             <EntrypointsStackedBarChart
               data={sortedEntrypoints}
-              className="mt-2"
+              className="my-3"
             />
           </div>
 
